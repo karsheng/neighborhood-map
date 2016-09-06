902dc8e6635906e0b2cbfe28ed9b366444409760
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-09-06 22:17:53 +0800

    added markers and stuff

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 //global variables
 var map;
+var marker;
 
 //initialize map
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  //temporary latlng for markers
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
   });
+
+  //temporary marker
+  marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+//make marker bounce
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
