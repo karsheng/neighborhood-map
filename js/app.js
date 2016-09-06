46af1c9f1f48331b2e02e0f8a05cec65048376bc
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2016-09-06 21:49:58 +0800

    included map

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//global variables
+var map;
+
+//initialize map
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
